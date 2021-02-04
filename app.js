e76b1d0d3f0b654e11e6b081ad56c09b9bb6aa26
ramiro-e <ramiroestevez96@gmail.com>
2021-02-04 18:03:34 -0300

    fourth commit

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static('publicPath'));
+app.use(express.static('public'));
 
 app.set('puerto', process.env.PORT|| 3000);
 
