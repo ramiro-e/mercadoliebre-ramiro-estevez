cedefb8a42f48d4c6d767cdad0c675b9b846e04a
ramiro-e <ramiroestevez96@gmail.com>
2021-02-04 14:12:58 -0300

    third commit

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const publicPath = path.resolve(dirname, './public')
+const publicPath = path.resolve(__dirname, './public');
 app.use(express.static('publicPath'));
 
 app.set('puerto', process.env.PORT|| 3000);
 
-app.listen(app.get('puerto'), () =>console.log('Servidor Corrindo'))
+app.listen(app.get('puerto'), () =>console.log(`Servidor Corrindo ${app.get('puerto')}`));
   
 app.get('/', function(req,res){
-    res.sendFile(path.resolve(__dirname, './views/index.html'))
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
 })
 
 app.get('/registro', function(req,res){
-    res.sendFile(path.resolve(__dirname, './views/register.html'))
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 })
 
 app.get('/login', function(req,res){
-    res.sendFile(path.resolve(__dirname, './views/login.html'))
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
